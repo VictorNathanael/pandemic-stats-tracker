e63c794c9c796679f87118c31e579d4c8805927a
Victor Nathanael <victornathanael15@gmail.com>
2023-03-09 22:25:54 -0300

    feat: add code to keep selected option selected

--- index.php
+++ index.php
@@ -6,33 +6,33 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>covid-data-visualizer</title>
         <link rel="stylesheet" href="/css/style.css" />
+        <link rel="icon" type="image/png" href="../../assets/img/icon_kidopi.png"/>
     </head>
     <body>
         <header>
             <img src="./assets/img/logo_kidopi.png" alt="logo kidopi">
             <h1>Consulta de casos de covid</h1>
+            <a href="./php/compare/index.php">
+                <button>Comparar taxas de mortalidade</button>
+            </a>
         </header>
         <main>
             <form method="post">
-                <label for="pais">Selecione um país:</label>
-                <select name="pais" id="pais" onchange="this.form.submit()">
-                    <option value="Brazil">Brasil</option>
-                    <option value="Canada">Canadá</option>
-                    <option value="Australia">Austrália</option>
-                </select>
+                    <select name="pais" id="pais" onchange="this.form.submit()">
+                        <option value="" selected disabled>Selecione um país</option>
+                        <option value="Brazil" <?php if(isset($_POST['pais']) && $_POST['pais'] == 'Brazil') { echo ' selected'; } ?>>Brasil</option>
+                        <option value="Canada" <?php if(isset($_POST['pais']) && $_POST['pais'] == 'Canada') { echo ' selected'; } ?>>Canadá</option>
+                        <option value="Australia" <?php if(isset($_POST['pais']) && $_POST['pais'] == 'Australia') { echo ' selected'; } ?>>Austrália</option>
+                    </select>
             </form>
             <div id="casosTotais">
-                <?php include './php/consulta.php'; ?>
+                <?php include './php/visualizer/consulta.php'; ?>
             </div>
         </main>
         <footer>
             <div id="ultimaConsulta">
                 <p>
-                    O último país procurado foi:
-                    <?php echo $_POST['pais'] ?? ''; ?>
-                </p>
-                <p>
-                    <?php include './php/ultimaConsulta.php'?>
+                    <?php include './php/visualizer/ultimaConsulta.php'?>
                 </p>
             </div>
         </footer>
