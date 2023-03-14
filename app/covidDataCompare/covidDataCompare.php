f9ffc446a0349852d2a3cc07ba09d0d22e1f8f18
Victor Nathanael <victornathanael15@gmail.com>
2023-03-14 18:09:18 -0300

    add HTML structure for covidDataCompare page

---
+++ app/covidDataCompare/covidDataCompare.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Covid Data Compare</title>
+        <link rel="icon" type="image/png" href="../assets/img/icon_kidopi.png"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./style.css" />
+    </head>
+    <body>
+        <header>
+            <img src="../assets/img/logo_kidopi.png" alt="logo kidopi">
+            <h1>Comparar taxas de mortalidade</h1>
+            <a href="../../index.php">
+                <button id="covidData">Consultar dados de covid</button>
+            </a>
+        </header>
+        <main>
+            <?php include './formCompareCountry.php'?>
+            <div id="mortalityRate">
+                <?php include './rateMortality.php' ?>
+            </div>
+        </main>
+        <footer>
+        </footer>
+        <script src="./covidDataCompare.js"></script>
+    </body>
+</html>
